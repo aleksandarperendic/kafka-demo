b866424ca0b3c52a7e04f70fcbaaf8ff981d0897
Aleksandar Perendic <aleksandar.perendic@ruter.no>
2022-08-14 11:56:07 +0200

    Grecefull shutdown

---
+++ kafka-basics/src/main/java/demos/kafka/ConsumerDemoWithShutDown.java
@@ -0,0 +1,84 @@
+package demos.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+public class ConsumerDemoWithShutDown {
+
+    private static final Logger log = LoggerFactory.getLogger(ConsumerDemoWithShutDown.class.getSimpleName());
+
+    public static void main(String[] args) {
+        log.info("I'm kafka producer");
+
+        String bootstrapServers = "127.0.0.1:9092";
+        String groupID = "my-thir-application";
+        String topic = "demo_java";
+
+        // Create consumer properties
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupID);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        // Create consumer
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+
+        // Get a rference to the current thread
+        final Thread mainThread = Thread.currentThread();
+
+        // Adding the shutdown hook
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            public void run() {
+                log.info("Detected shutdown, let´s exit by calling consumer.wakeup() ");
+                consumer.wakeup();
+
+                // Join the main thread to allow the execution of the code in the main thread
+                try {
+                    mainThread.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        try{
+
+        // Subscribe consumer to our topic(s)
+        consumer.subscribe(Arrays.asList(topic));
+
+        // Poll for new data
+        while (true) {
+            log.info("Polling");
+
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(1000));
+
+
+            for (ConsumerRecord<String, String> record : records) {
+                log.info("Key: " + record.key() + ", Value: " + record.value());
+                log.info("Partition: " + record.partition() + ", Offset: " + record.offset());
+            }
+
+        }
+    } catch (WakeupException e) {
+            log.info("Wake up exception");
+            // we ignore this
+        } catch (Exception e) {
+            log.error("Unexpected exception");
+        } finally {
+            consumer.close(); // This will also commit the offsets if needed
+            log.info("The consumer is now gracefully closed");
+        }
+    }
+}
